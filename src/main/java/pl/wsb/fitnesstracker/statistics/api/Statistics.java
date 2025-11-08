6a51db7086e306366a31275075b3dd168c847519
xHeput <maciek.niemiec9@wp.pl>
2025-11-08 15:50:13 +0100

    adding new class HealthMetrics.java and fixing Statistics

--- src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
+++ src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
@@ -1,36 +1,26 @@
 package pl.wsb.fitnesstracker.statistics.api;
 
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
+import pl.wsb.fitnesstracker.user.api.User;
 @Entity
-@Table(name = "Statistics")
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
+@Table(name = "statistics")
 public class Statistics {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Nullable
     private Long id;
 
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @Column(name = "total_trainings", nullable = false)
-    private int totalTrainings;
+    private Integer totalTrainings;
 
     @Column(name = "total_distance")
-    private double totalDistance;
+    private Double totalDistance;
 
     @Column(name = "total_calories_burned")
-    private int totalCaloriesBurned;
+    private Integer totalCaloriesBurned;
 
-    public Statistics(int totalTrainings, double totalDistance, int totalCaloriesBurned) {
-        this.totalTrainings = totalTrainings;
-        this.totalDistance = totalDistance;
-        this.totalCaloriesBurned = totalCaloriesBurned;
-    }
 }
